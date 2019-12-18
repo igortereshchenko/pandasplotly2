2af83ce9763512d8417ec90f5fd30e8ee6e0d84c
Sonya <55147091+pobiedimska@users.noreply.github.com>
2019-12-18 12:23:51 +0200

    last-last version of bar

--- km93/pobiedimskasi/main.py
+++ km93/pobiedimskasi/main.py
@@ -1,5 +1,6 @@
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="keys.json"
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys.json"
 
 import pandas as pd
 from bq_helper import BigQueryHelper
@@ -8,43 +9,36 @@ from plotly.offline import plot
 
 bq_assistant = BigQueryHelper('bigquery-public-data', 'epa_historical_air_quality')
 
-
 QUERY = """
         SELECT state_name, year, observation_percent, sample_duration
         FROM `bigquery-public-data.epa_historical_air_quality.air_quality_annual_summary`
         LIMIT 100
         """
 
-
 df = bq_assistant.query_to_pandas(QUERY)
-
 
 print(df.head(3))
 
-df_observation_state = df.groupby(['year'])['observation_percent'].mean()
+df_observation_sample_duration = df.groupby(['sample_duration'])['observation_percent'].mean()
 
 trace1 = go.Scatter(
 
 )
 
-
-
 trace2 = go.Pie(
 
-                    )
-
-trace3 = go.Bar(
-x = df_observation_state.index,
-    y = df_observation_state.values,
-    name = 'Observation percent depending on state'
 )
 
-data = [trace3]
+trace3 = go.Bar(
+    x=df_observation_sample_duration.index,
+    y=df_observation_sample_duration.values,
+    name='Observation percent depending on sample duration'
+)
 
-layout = dict(
-              title = 'Observations',
-              xaxis= dict(title= 'year'),
-              yaxis=dict(title='average observation percent'),
-             )
-fig = dict(data = [trace3], layout = layout)
+data3 = [trace3]
+
+layout3 = dict(title='Observations', xaxis=dict(title='sample duration'),
+               yaxis=dict(title='average observation percent'))
+
+fig = dict(data=data3, layout=layout3)
 plot(fig)
