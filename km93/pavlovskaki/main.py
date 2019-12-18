4476b5b3e16e37f8c0f39c29d613901119f66fcd
Kate Pavlovska <pavlovskakate2002@gmail.com>
2019-12-18 11:41:27 +0200

    Scatter

--- km93/pavlovskaki/main.py
+++ km93/pavlovskaki/main.py
@@ -19,14 +19,27 @@ QUERY = """
 df = bq_assistant.query_to_pandas(QUERY)
 
 _sample_measurement_count = df.groupby(['time_local'])['sample_measurement'].count()
+__local_count = df.groupby(['time_local'])['state_code'].count()
 
 trace1 = go.Bar(
     x=_sample_measurement_count.index,
     y=_sample_measurement_count.values
 )
+
+trace2 = go.Scatter(
+    x=__local_count.index,
+    y=__local_count.values,
+    mode='lines'
+                    )
 layout1 = go.Layout(
     title='Код Fibs відносно виміряних значень',
     xaxis=dict(title='Виміряні значення'),
     yaxis=dict(title='Код Fibs')
 )
+layout2 = go.Layout(
+    title='Fibs відносно часу дня',
+    xaxis=dict(title='Час дня'),
+    yaxis=dict(title='Код Fibs'),
+)
 figure1 = go.Figure(data=[trace1], layout=layout1)
+figure2 = go.Figure(data=[trace1], layout=layout1)
