cbd64af422b6154cd6585f834674068f8a2b61b5
pinkLama <eclipse7723@gmail.com>
2019-12-18 11:59:28 +0200

    Scatter

---
+++ km93/dovgaleo/main.py
@@ -0,0 +1,34 @@
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys.json"
+
+import pandas as pd
+from bq_helper import BigQueryHelper
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+bq_assistant = BigQueryHelper('epa_historical_air_quality', 'co_hourly_summary')
+
+QUERY = """
+        SELECT `state_code`, `date_local`, `sample_measurement`, `units_of_measure` 
+        FROM `bigquery-public-data.epa_historical_air_quality.co_hourly_summary`
+        LIMIT 1000
+        """
+
+df = bq_assistant.query_to_pandas(QUERY)
+
+trace1 = go.Scatter(
+    x=df['date_local'],
+    y=df['sample_measurement']
+)
+
+data = [trace1]
+
+layout = dict(
+    title='Measurement per day in millions',
+    xaxis=dict(title='Date'),
+    yaxis=dict(title='Measurement per million')
+)
+
+fig = dict(data=[trace1], layout=layout)
+plot(fig)
