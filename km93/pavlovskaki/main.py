e27cfde7ef392620851f524b35d633c70665b107
Kate Pavlovska <pavlovskakate2002@gmail.com>
2019-12-18 11:49:48 +0200

    Pie

--- km93/pavlovskaki/main.py
+++ km93/pavlovskaki/main.py
@@ -20,6 +20,7 @@ df = bq_assistant.query_to_pandas(QUERY)
 
 _sample_measurement_count = df.groupby(['time_local'])['sample_measurement'].count()
 __local_count = df.groupby(['time_local'])['state_code'].count()
+__units_of_measure__count = df.groupby(['time_local'])['units_of_measure'].count()
 
 trace1 = go.Bar(
     x=_sample_measurement_count.index,
@@ -31,6 +32,10 @@ trace2 = go.Scatter(
     y=__local_count.values,
     mode='lines'
                     )
+trace3 = go.Pie(
+    labels=__units_of_measure__count.index,
+    values=__units_of_measure__count.values,
+)
 layout1 = go.Layout(
     title='Код Fibs відносно виміряних значень',
     xaxis=dict(title='Виміряні значення'),
@@ -41,5 +46,7 @@ layout2 = go.Layout(
     xaxis=dict(title='Час дня'),
     yaxis=dict(title='Код Fibs'),
 )
+
 figure1 = go.Figure(data=[trace1], layout=layout1)
 figure2 = go.Figure(data=[trace2], layout=layout2)
+figure3 = go.Figure(data=[trace3])
