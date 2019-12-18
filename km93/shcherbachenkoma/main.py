062f0a1c61a1ccaf1661e554651155f7e98cc76e
MaximShcherbachenko <55317211+MaximShcherbachenko@users.noreply.github.com>
2019-12-18 12:14:25 +0200

    bar

---
+++ km93/shcherbachenkoma/main.py
@@ -0,0 +1,45 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="keys.json"
+
+
+import pandas as pd
+from bq_helper import BigQueryHelper
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+bq_assistant = BigQueryHelper("bigquery-public-data", "epa_historical_air_quality")
+
+
+QUERY = """
+        SELECT state_code, sample_duration, date_local, observation_count
+        FROM `bigquery-public-data.epa_historical_air_quality.lead_daily_summary`
+        LIMIT 10
+        """
+
+
+df = bq_assistant.query_to_pandas(QUERY)
+print(df)
+
+
+
+
+trace1 = go.Scatter(
+                        x=df["sample_duration"].index,
+                        y=df["date_local"],
+                        mode="lines"
+
+                    )
+
+
+
+
+
+data = [trace1]
+
+layout = dict(
+              title = '',
+              xaxis= dict(title= ''),
+              yaxis=dict(title=''),
+             )
+fig = dict(data = [trace1], layout = layout)
+plot(fig)
