57cd82299aab8217780e8817f20e0216004ae37c
idont111 <55505572+idont111@users.noreply.github.com>
2019-12-18 11:56:24 +0200

    scatter

--- km93/chernyshaa/main.py
+++ km93/chernyshaa/main.py
@@ -10,7 +10,7 @@ bq_assistant = BigQueryHelper('bigquerry-public-data','pm10_daily_summary')
 
 
 QUERY = """
-        SELECT county_code, sample_duration, date_local, observation_count  FROM 'bigquerry-public-data._pm10_daily_summary'
+        SELECT county_code, sample_duration, date_local, observation_count  FROM `bigquerry-public-data._pm10_daily_summary`
         LIMIT 100
         """
 
@@ -18,7 +18,15 @@ QUERY = """
 df = bq_assistant.query_to_pandas(QUERY)
 
 
-local_count = df.grouphy(['county_code'])['observation_count'].count()
+local_count_1 = df.grouphy(['county_code'])['observation_count'].count()
+local_count_2 = df.grouphy(['county_code'])['observation_count'].count()
+
+trace2 = go.Scatter(
+    x=local_count.index,
+    y=local_count.value,
+    marker=dict(color='rgba(225, 174, 255, 0.5)'
+
+)
 
 trace3 = go.Bar(
     x = local_count.index,
@@ -26,11 +34,19 @@ trace3 = go.Bar(
     marker = dict(color = 'rgba(225, 174, 255, 0.5)'
 )
 
-layout = go.Layout(
+layout1 = go.Layout(
+              title = 'Observation_count relatively county_code',
+              xaxis = dict(title= 'county_code'),
+              yaxis = dict(title='observation_count')
+)
+
+layout2 = go.Layout(
               title = 'Observation_count relatively county_code',
               xaxis = dict(title= 'county_code'),
               yaxis = dict(title='observation_count')
 )
 
 
-figure1 = go.Figure(data = [trace3], layout = layout)
+
+figure1 = go.Figure(data = [trace3], layout = layout1)
+figure2 = go.Figure(data = [trace2], layout = layout2)
