c165e9c10bf19eb0368c220d6c646016bcf9051f
pankeeva <pankeeva@gmail.com>
2019-12-18 12:39:24 +0200

    scatter

--- km93/pankeyevasd/main.py
+++ km93/pankeyevasd/main.py
@@ -11,12 +11,12 @@ bq_assistant = BigQueryHelper('epa_historical_air_quality' ,'bigquery-public-dat
 
 QUERY = """
         SELECT state_code, sample_duration, date_local,  units_of_measure  FROM bigquery-public-data.epa_historical_air_quality.lead_daily_summary
-        LIMIT 300
+        LIMIT 100
         """
 
 _sample_duration_count = df.groupby(['date_local'])['sample_duration'].count()
 _units_of_measure_count = df.groupby(['date_local'])['units_of_measure'].count()
-
+_code_count = df.groupby(['date_local'])['code_count'].count()
 trace1 = go.Bar(
     x=_sample_duration_count.index,
     y=_sample_duration_count.values
@@ -25,13 +25,25 @@ trace2 = go.Pie(
     x=_units_of_measure_count.index,
     y=_units_of_measure_count.values
 )
+trace3 = go.Scatter(
+    x=_code_count.index,
+    y=_code_count.values,
+    mode='lines'
+)
 layout1 = go.Layout(
-    title='Дата відносно часу ',
+    title='Дата відносно часу',
     xaxis=dict(title='Тривалість часу'),
-    yaxis=dict(title=' Дата ')
-
+    yaxis=dict(title='Дата')
+)
+layout3 = go.Layout(
+    title='Стан  відносно дати',
+    xaxis=dict(title='дата'),
+    yaxis=dict(title='Стан')
+)
 
 figure1 = go.Figure(data=[trace1], layout=layout1)
 plot(figure1)
 figure2 = go.Figure(data=[trace2], layout=layout2)
 plot(figure2)
+figure3 = go.Figure(data=[trace2], layout=layout2)
+plot(figure3)
