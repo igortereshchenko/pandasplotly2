57d6c7a6df4f6740396b10b9b6d1079ea314fdbe
idont111 <55505572+idont111@users.noreply.github.com>
2019-12-18 12:02:49 +0200

    Pie

--- km93/chernyshaa/main.py
+++ km93/chernyshaa/main.py
@@ -20,25 +20,30 @@ df = bq_assistant.query_to_pandas(QUERY)
 
 local_count_1 = df.grouphy(['county_code'])['observation_count'].count()
 local_count_2 = df.grouphy(['county_code'])['observation_count'].count()
+local_count_3 = df.grouphy(['county_code'])['observation_count'].count()
 
-
+trace3  = go.Scatter(
+    x = local_count_3.index,
+    y = local_count_3.value,
+    marker=dict(color='rgba(225, 174, 255, 0.5)')
+)
 trace2 = go.Scatter(
-    x=local_count_2.index,
-    y=local_count_2.value,
-    marker=dict(color='rgba(225, 174, 255, 0.5)'
+    x = local_count_2.index,
+    y = local_count_2.value,
+    marker=dict(color='rgba(225, 174, 255, 0.5)')
 
 )
 
-trace3 = go.Bar(
+trace1 = go.Bar(
     x = local_count_1.index,
     y = local_count_1.value,
-    marker = dict(color = 'rgba(225, 174, 255, 0.5)'
+    marker = dict(color = 'rgba(225, 174, 255, 0.5)')
 )
 
-layout1 = go.Layout(
+layout3 = go.Layout(
               title = 'Observation_count relatively county_code',
-              xaxis = dict(title= 'county_code'),
-              yaxis = dict(title='observation_count')
+              xaxis = dict(title = 'county_code'),
+              yaxis = dict(title = 'observation_count')
 )
 
 layout2 = go.Layout(
@@ -49,5 +54,6 @@ layout2 = go.Layout(
 
 
 
-figure1 = go.Figure(data = [trace3], layout = layout1)
+figure1 = go.Figure(data = [trace1], layout = layout1)
 figure2 = go.Figure(data = [trace2], layout = layout2)
+figure3 = go.Figure(data = [trace3])
