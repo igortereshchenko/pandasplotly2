a43230512883f8760448e732d7dcb9e316175a53
pinkLama <eclipse7723@gmail.com>
2019-12-18 12:27:36 +0200

    Pie

--- km93/dovgaleo/main.py
+++ km93/dovgaleo/main.py
@@ -21,20 +21,32 @@ trace1 = go.Scatter(
     x=df['date_local'],
     y=df['sample_measurement']
 )
+
 trace2 = go.Bar(
     x=df['date_local'],
     y=df['sample_measurement']
 )
+
 trace3 = go.Pie(
+    values=df['sample_measurement'],
+    labels=df['date_local']
 )
 
-data = [trace2]
-
-layout = dict(
+layout1 = dict(
     title='Measurement per day in millions',
     xaxis=dict(title='Date'),
     yaxis=dict(title='Measurement per million')
 )
 
-fig = dict(data=[trace2], layout=layout)
-plot(fig)
+layout2 = dict(
+    title='Measurement per day in millions',
+    xaxis=dict(title='Date'),
+    yaxis=dict(title='Measurement per million')
+)
+
+fig1 = dict(data=[trace1], layout=layout1)
+fig2 = dict(data=[trace2], layout=layout2)
+fig3 = dict(data=[trace3])
+plot(fig1)
+plot(fig2)
+plot(fig3)
